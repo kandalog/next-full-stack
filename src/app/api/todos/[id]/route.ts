5fdd2bc27cac7fc4c34803b3134e4d66a8fbadce
kandalog <asuasu.tee@gmail.com>
2025-06-09 23:24:09 +0900

    Update API

---
+++ src/app/api/todos/[id]/route.ts
@@ -0,0 +1,67 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+
+export async function PATCH(
+  _req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const { id } = params;
+
+    const todo = await prisma.todo.findUnique({
+      where: { id: Number(id) },
+    });
+
+    if (!todo) {
+      return NextResponse.json(
+        { error: "todoが見つかりません" },
+        { status: 404 }
+      );
+    }
+
+    await prisma.todo.update({
+      where: { id: Number(id) },
+      data: { completed: !todo.completed },
+    });
+
+    return NextResponse.json({ message: "success", todo });
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json(
+      { error: "todoの更新に失敗しました" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(
+  _req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const { id } = params;
+
+    const todo = await prisma.todo.findUnique({
+      where: { id: Number(id) },
+    });
+
+    if (!todo) {
+      return NextResponse.json(
+        { error: "todoが見つかりません" },
+        { status: 404 }
+      );
+    }
+
+    await prisma.todo.delete({
+      where: { id: Number(id) },
+    });
+
+    return NextResponse.json({ message: "success", todo });
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json(
+      { error: "todoの削除に失敗しました" },
+      { status: 500 }
+    );
+  }
+}
